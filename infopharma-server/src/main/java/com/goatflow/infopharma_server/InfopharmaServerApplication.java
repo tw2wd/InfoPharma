7b0da9f842c7b21db316d77d616062c8f2c13d4d
tw2wd <jennifer.green.4@city.ac.uk>
2026-02-10 13:27:08 +0000

    first backend commit

---
+++ infopharma-server/src/main/java/com/goatflow/infopharma_server/InfopharmaServerApplication.java
@@ -0,0 +1,13 @@
+package com.goatflow.infopharma_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfopharmaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfopharmaServerApplication.class, args);
+	}
+
+}
---
+++ infopharma-server/src/test/java/com/goatflow/infopharma_server/InfopharmaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goatflow.infopharma_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfopharmaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
